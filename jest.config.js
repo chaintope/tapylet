f9d7e1d690e06abd9b4b15b0cfb24cb702a7426b
azuchi <azuchi@chaintope.com>
2026-02-16 14:11:22 +0900

    Add .pnpm to transformIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
     }],
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(@noble|tapyrusjs-lib|bip39|bip32)/)',
+    'node_modules/(?!(\\.pnpm|@noble|tapyrusjs-lib|bip39|bip32)/)',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
 };
